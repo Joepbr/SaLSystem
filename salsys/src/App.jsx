e7aab8c26ce8f9fcb8c92860664f8ab5e63c6f76
Joel Barini <joepb_008@hotmail.com>
2024-03-15 20:09:45 -0300

    (15/03/24) Criação da página de Login

--- salsys/src/App.jsx
+++ salsys/src/App.jsx
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CssBaseline } from '@mui/material'
 import Drawer from './ui/Drawer'
+import Container from '@mui/material/Container'
 
 import Homepage from './pages/Homepage'
 import Cursos from './pages/CursoList'
@@ -13,39 +14,73 @@ import Profs from './pages/ProfList'
 import Alunos from './pages/AlunoList'
 import Aulas from './pages/AulaList'
 
+import LoginForm from './ui/LoginForm'
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <CssBaseline /> {/* Reseta o CSS padrão e adapta para o tema */}
-        <Box sx={{
-          width: '100vw', //toda a alrgura da janela
-          minHeight: '100vh', //no mínimo, toda a altura da janela
-          //cinza escuro, no modo dark
-          backgroundColor: '#d9d9d9',
-          color: 'black',
-        }}>
-          <Drawer/>
-          <Box 
-            sx={{ 
-              marginLeft: '250px',
-              marginTop: '100px',
-            }}>
+        <CssBaseline />
             <Routes>
-            <Route path="/" element={ <Homepage /> } />
-            <Route path="/cursos" element={ <Cursos /> } />
-            <Route path="/profs" element={ <Profs /> } />
-            <Route path="/alunos" element={ <Alunos /> } />
-            <Route path="/aulas" element={ <Aulas /> } />
+
+            <Route path="/login" element={<LoginPage/>} />
+
+            <Route path="/" element={<ProtectedPage />} >
+              <Route index element={ <Homepage /> } />
+              <Route path="/cursos" element={ <Cursos /> } />
+              <Route path="/profs" element={ <Profs /> } />
+              <Route path="/alunos" element={ <Alunos /> } />
+              <Route path="/aulas" element={ <Aulas /> } />
+            </Route>
           </Routes>
-          </Box>
-        </Box>
       </ThemeProvider>
       </BrowserRouter>
     </>
-  )
+  );
+}
+
+function ProtectedPage({ children, ...props }) {
+  return (
+    <Box 
+      sx={{
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundColor: '#d9d9d9',
+        color: 'black',
+      }}
+      {...props}
+    >
+      <Drawer/>
+        <Box 
+          sx={{ 
+            marginLeft: '250px',
+            marginTop: '100px',
+          }}
+        >
+          {children}
+        </Box>
+    </Box>
+  );
+}
+
+function LoginPage() {
+  return (
+    <Box
+      sx={{
+        backgroundColor: '#25254b',
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Container component="main" maxWidth="xs">
+        <LoginForm />
+      </Container>
+    </Box>
+  );
 }
 
 export default App
---
+++ salsys/src/ui/LoginForm.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import myfetch from '../utils/myfetch'; // Import myfetch.js file
+import { Button, CssBaseline, TextField, Box, Typography, Container, ThemeProvider } from '@mui/material';
+import logo from '../assets/Sallogo.png';
+import theme from '../utils/theme'
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  // create error states
+  const [usernameError, setUsernameError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await myfetch.post('/login', { username, password });
+      // If login successful, redirect or perform desired action
+      console.log('Login successful:', response);
+    } catch (error) {
+      console.error('Login failed:', error);
+      // Handle login error (display error message, reset form, etc.)
+      setUsernameError('Wrong username or password');
+      setPasswordError('Wrong username or password');
+    }
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+            aria-label="Login form"
+          >
+          <img src={logo} alt="Logotipo SaL" style={{ width: '325px'}}/>
+          <Typography 
+            component="h1" 
+            variant="h5" 
+            sx={{
+              mt: 3, 
+              mb: 2, 
+              fontFamily: "Impact",
+              fontSize: 53,
+              textShadow: '-4px 4px 0 #9d2f2e, 4px 4px 0 #9d2f2e, 4px -4px 0 #9d2f2e, -4px -4px 0 #9d2f2e',
+              textAlign: "center",
+              }}
+          >
+            Bem Vindo ao Sistema SaL!
+          </Typography>
+          <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }} aria-labelledby="login-form">
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Usuário"
+              name="username"
+              autoComplete="usuário"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              error={usernameError != null}
+              helperText={usernameError}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              error={passwordError != null}
+              helperText={passwordError}
+            />
+            <Button 
+              type="submit"
+              size='large'
+              fullWidth
+              variant="contained"
+              sx={{
+                fontFamily: "Impact",
+                textShadow: '-2px 2px 0 #104978, 2px 2px 0 #104978, 2px -2px 0 #104978, -2px -2px 0 #104978',
+                backgroundColor: "#9d2f2e",
+                fontSize: 25, 
+                mt: 2, 
+                mb: 2, 
+              }}
+              aria-label="Click to login"
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default LoginForm;
