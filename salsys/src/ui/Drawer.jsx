f9e559fd8e4b7120dfc2625dd01c38035d339660
Joel Barini <joepb_008@hotmail.com>
2024-03-18 19:57:33 -0300

    (18/03/24) Tentativa de conecter com back-end2

--- salsys/src/ui/Drawer.jsx
+++ salsys/src/ui/Drawer.jsx
@@ -1,15 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import { Box, Drawer, CssBaseline, AppBar, Toolbar, List, Typography, Divider, ListItem, ListItemButton, ListItemText} from '@mui/material';
 import logo from '../assets/Sallogo.png';
 import { Link, useLocation } from 'react-router-dom';
 
